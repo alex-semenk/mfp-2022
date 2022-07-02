4c870b7cf48270bf2d489dd85058fb924f6a8b0e
Aliaksandr SEMIANKEVICH <Aliaksandr.Semiankevich@prt.manomano.com>
2022-07-02 17:09:56 +0400

    Task 2 - Add kafkaListenerContainerFactory to KafkaConfiguration

--- resource.processor/src/main/java/com/mfp/resource/processor/KafkaConfiguration.java
+++ resource.processor/src/main/java/com/mfp/resource/processor/KafkaConfiguration.java
@@ -1,7 +1,10 @@
 package com.mfp.resource.processor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.support.converter.MessageConverter;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 
@@ -14,6 +17,16 @@ public class KafkaConfiguration {
     @Bean
     public MessageConverter messageConverter() {
         return new StringJsonMessageConverter();
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+        @Autowired ConsumerFactory<String, String> consumerFactory,
+        @Autowired MessageConverter messageConverter) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
     }
 
 }
